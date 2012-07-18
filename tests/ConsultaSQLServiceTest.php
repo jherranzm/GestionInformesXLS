5ff05b31fdf160b8d0162808ffcc8f6d22b5077e
José Luis Herranz <jherranzm@gmail.com>
2012-07-18 14:01:00 +0200

    Cambio en un test.

--- tests/ConsultaSQLServiceTest.php
+++ tests/ConsultaSQLServiceTest.php
@@ -1,61 +1,30 @@
 <?php
     
 require_once('ConsultaSQLService.php');
-require_once('InformeXLSService.php');
 
 class ConsultaSQLServiceTest  extends PHPUnit_Framework_TestCase{
     
     public function testConsultaSQLGetById(){
         $idConsulta = 1;
         $svc = new ConsultaSQLService();
-        $cons = $svc->getById($idConsulta);   
-        $this->assertEquals("977R.getAcuerdos", $cons->nombre);
+        $obj = $svc->getById($idConsulta);   
+        $this->assertEquals("977R.getAcuerdos", $obj->nombre);
     }
 
 
-    public function testInformeXLSGetById(){
-        $id = 1;
-        $svc = new InformeXLSService();
-        $inf = $svc->getById($id);   
-        $this->assertEquals("AcuerdoAplicado", $inf->nombre);
-    }
-    
-    public function testInformeXLSSave(){
-        $nuevoinforme = new InformeXLS();
-        $this->assertEquals("", $nuevoinforme->nombre);
+    public function testConsultaSQLSave(){
+        $_nombre = "NuevaConsulta".rand(1,234);
+        $obj = new ConsultaSQL();
+        $this->assertEquals("", $obj->nombre); // Nombre vacio por defecto
+        $obj->nombre = $_nombre;
         
-        $svc = new InformeXLSService();
-        $inf = $svc->save($nuevoinforme);   
-        echo $inf.""."\n";
-        $this->assertGreaterThan(0, $inf);
-    }
-    
-    public function testSuper(){
-        $idInforme = 1;
-        $svc = new InformeXLSService();
-        $informe = $svc->getById($idInforme);   
+        $svc = new ConsultaSQLService();
+        $_id = $svc->save($obj);   
+        echo "".$_id.""."\n";
+        $this->assertGreaterThan(0, $_id);
 
-        echo $idInforme." : ".$informe->nombre."\n"; 
-        
-        $informe2 = new InformeXLS();
-        $informe2->nombre = "Nuevo Informe";
-        $res = $svc->save($informe2);
-        
-        echo $res."\n"; 
-        $informe = $svc->getById($res);
-        echo $idInforme." : ".$informe->nombre."\n"; 
-        
-        $informe->nombre = $informe->nombre."".rand(1,234);
-
-        $f = $svc->update($informe);
-        
-        assert($f);
-        $informe3 = $svc->getById($res);
-        echo $idInforme." : ".$informe3->nombre."\n"; 
-        
-        $res = $svc->delete($res);
-        echo $res."\n"; 
-        
+        $obj = $svc->getById($_id);   
+        $this->assertEquals($_nombre, $obj->nombre);
     }
     
 }
